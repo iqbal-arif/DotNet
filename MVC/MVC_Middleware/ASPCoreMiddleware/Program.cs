85263a0ca0abfeaf84287af051f1fefe72071456
Arif Iqbal <arif_iqbal@yahoo.com>
2024-08-07 12:56:54 -0500

    Added MVC_Routing from Scratch in ASPCoreMiddleware solution

--- MVC/MVC_Middleware/ASPCoreMiddleware/Program.cs
+++ MVC/MVC_Middleware/ASPCoreMiddleware/Program.cs
@@ -5,29 +5,43 @@ namespace ASPCoreMiddleware
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //Required to register MVC Controllers and Views
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
+
+            app.MapDefaultControllerRoute();
+            //START ---- FOR ROUTNING ----START
+            //Activate when not implementing MVC model
             //Hello World ; Simple MIddleware route
             //app.MapGet("/", () => "Hello World!");
 
+            //Activate when not implementing MVC model
             //One Middleware statement
             //app.Run(async(context) =>
             //{
             //    await context.Response.WriteAsync("Welcome to ASP.NET Core 8");
             //});
-            //Use method is used to excute multiple middleware in order
-            app.Use(async (context,next) =>
-            {
-                await context.Response.WriteAsync("Welcome to ASP.NET Core 8");
-                await next(context); 
-            });
 
+            //Activate when not implementing MVC model, and
+            //Use method is used to excute multiple middleware in order 
+            //First Middleware
+            //app.Use(async (context,next) =>
+            //{
+            //    await context.Response.WriteAsync("Middleware 1: Welcome to ASP.NET Core 8");
+            //    await next(context); 
+            //});
+
+            //Activate when not implementing MVC model, and
             //Run method is used to run a single middleware
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("The second context after running the first middleware");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Middleware 2: The second context after running the first middleware");
+            //});
+            //END ---- FOR ROUTNING ----END
 
-            app.Run();
+            app.Run(); //To Run App.
         }
     }
 }
